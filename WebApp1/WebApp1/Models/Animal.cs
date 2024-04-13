a6a2779c337e8e5ea5cbb6b77ac885aa13953062
Rzelek <55500426+Rzelekk@users.noreply.github.com>
2024-04-13 19:40:32 +0200

    add parameters and constructor

--- WebApp1/WebApp1/Models/Animal.cs
+++ WebApp1/WebApp1/Models/Animal.cs
@@ -1,7 +1,19 @@
-namespace Tutorial4.Models;
+namespace WebApp1.Models;
 
 public class Animal
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string Category { get; set; }
+    public float Mass { get; set; }
+    public string Colour { get; set; }
+
+    public Animal(int id, string name, string category, float mass, string colour)
+    {
+        Id = id;
+        Name = name;
+        Category = category;
+        Mass = mass;
+        Colour = colour;
+    }
 }
